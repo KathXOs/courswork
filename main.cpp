29df044a830b561d79c6c8fa3c257750480b433b
Ekaterina Oskina <katty.nm@mail.ru>
2018-05-11 22:38:28 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+class Marka 
+{
+public:
+	virtual int createM();
+	
+}
+
+class realM public Marka
+{
+public:
+	int createM ();
+}
+
+class virtM public Marka
+{
+public:
+	int createM ();
+}
+
+int main ()
+{
+	int M;
+	cout << "vvedite kol-vo marok"<< endl;
+	cin >> N;
+	char ** P = new char *[N];
+	for (int i = 0; i <= Ns; i++)
+	P[i] = new char[300];
+	
+}
